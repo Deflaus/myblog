624e377dbbdbcf364532a86bbdb158cd17d813b8
DeFlaus <snow41521@gmail.com>
2020-08-02 16:32:50 +0300

    Добавление подсистемы тэгов

--- app_blog/views.py
+++ app_blog/views.py
@@ -3,11 +3,19 @@ from .models import Post
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .forms import EmailPostForm, CommnetForm
+from taggit.models import Tag
+from django.db.models import Count
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     published_posts = Post.published.all()
     object_list = Post.published.all()
+
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     # По 3 статьи на каждой странице.
     paginator = Paginator(object_list, 3)
     page = request.GET.get('page')
@@ -18,11 +26,13 @@ def post_list(request):
     # Если страница не является целым числом, возвращаем первую страницу.
         published_posts = paginator.page(1)
     except EmptyPage:
-    # Если номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+    # Если номер страницы больше, чем общее количество страниц, 
+    # возвращаем последнюю.
         published_posts = paginator.page(paginator.num_pages)
-    return render(request,
-        'app_blog/post/list.html',
-        {'page': page, 'published_posts': published_posts})
+    return render(request, 'app_blog/post/list.html',
+        {'page': page, 
+        'published_posts': published_posts,
+        'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
@@ -44,11 +54,15 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommnetForm()
 
+    post_tags_id = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_id).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-date_published')[:4]
     return render(request, 'app_blog/post/detail.html',
         {'post': post,
         'comments': comments,
         'new_comment': new_comment,
-        'comment_form': comment_form, })
+        'comment_form': comment_form,
+        'similar_posts': similar_posts, })
 
 
 def post_share(request, post_id):
--- myblog/settings.py
+++ myblog/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_blog.apps.AppBlogConfig',
     'django.contrib.postgres',
+    'taggit',
 ]
 
 MIDDLEWARE = [

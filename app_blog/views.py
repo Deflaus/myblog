d921cd9b15f1c293edf7e525d1a0db2ba06d158a
DeFlaus <snow41521@gmail.com>
2020-08-02 16:09:59 +0300

    Добавление подсистемы комментариев

--- app_blog/views.py
+++ app_blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommnetForm
 
 
 def post_list(request):
@@ -32,7 +32,23 @@ def post_detail(request, year, month, day, post):
         date_published__year=year,
         date_published__month=month,
         date_published__day=day)
-    return render(request, 'app_blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommnetForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommnetForm()
+
+    return render(request, 'app_blog/post/detail.html',
+        {'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form, })
 
 
 def post_share(request, post_id):
